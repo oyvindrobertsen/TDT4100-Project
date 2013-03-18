f727fdaeb0b6b66b24cabde67e004ebc698cb1a9
Øyvind Robertsen <me@oyvindrobertsen.com>
2013-03-18 20:02:53 +0100

    Does some rendering now

--- src/gui/MainDisplay.java
+++ src/gui/MainDisplay.java
@@ -5,9 +5,12 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
 
-public class MainDisplay extends BasicGame{
+public class MainDisplay extends BasicGame {
+	
+	private TiledMap map;
 
 	public MainDisplay(String title) {
 		super(title);
@@ -15,12 +18,12 @@ public class MainDisplay extends BasicGame{
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		
+		map.render(0,0,10,10,800,600);
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		
+		map = new TiledMap("res/Firsttest.tmx");
 	}
 
 	@Override
@@ -30,7 +33,7 @@ public class MainDisplay extends BasicGame{
 	
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new MainDisplay("PlatformGame"));
-		app.setDisplayMode(800, 600, true);
+		app.setDisplayMode(800, 600, false); 
 		app.start();
 	}
 
