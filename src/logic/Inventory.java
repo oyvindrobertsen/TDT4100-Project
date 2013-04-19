f5920b438789c3ef29eb4c9174eb2257135a52c1
Mathias Ose <mathias.ose@gmail.com>
2013-04-19 20:10:41 +0200

    bubblesort for inventory

--- src/logic/Inventory.java
+++ src/logic/Inventory.java
@@ -21,6 +21,7 @@ public class Inventory {
 			}
 		}
 		items.add(arg);
+		sort();
 	}
 	
 	public String toString() {
@@ -29,5 +30,23 @@ public class Inventory {
 			out += i + " (" + i.getCount() + ")\t";
 		}
 		return out;
+	}
+	
+	private void sort(){
+		for (int i = 0; i < items.size() - 1; i++) {
+			boolean sorted = true;
+
+			for (int j = 1; j < items.size() - i; j++) {
+				if ( items.get(j).toString().compareTo( items.get(j-1).toString() ) < 0 ) {
+					InventoryObject temp = items.get(j);
+					items.set(j, items.get(j-1));
+					items.set(j-1, temp);
+
+					sorted = false;
+				}
+			}
+
+			if (sorted) break;
+		}
 	}
 }
