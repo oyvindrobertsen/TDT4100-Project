5ee5f01987ecdadfd2a8332a3082910e1679bd62
Mathias Ose <mathias.ose@gmail.com>
2013-04-19 19:48:16 +0200

    InventoryObjects stack

--- src/logic/Inventory.java
+++ src/logic/Inventory.java
@@ -2,20 +2,31 @@ package logic;
 
 import java.util.ArrayList;
 
+import org.lwjgl.Sys;
+
 public class Inventory {
 	private ArrayList<InventoryObject> items = new ArrayList<InventoryObject>();
 	
-	public Inventory() {	
+	public Inventory() {
 	}
 	
-	public void addItem(InventoryObject i) {
-		items.add(i);
+	public void addItem(InventoryObject arg) {
+		for ( InventoryObject itm : items){
+			if ( itm.toString().equals( arg.toString() ) ){
+				int i = items.indexOf( itm );
+				itm = items.get(i);
+				itm.increment();
+				items.set(i, itm);
+				return;
+			}
+		}
+		items.add(arg);
 	}
 	
 	public String toString() {
 		String out = "Inventory:\t";
 		for (InventoryObject i : items) {
-			out += i + "\t";
+			out += i + " (" + i.getCount() + ")\t";
 		}
 		return out;
 	}
--- src/logic/InventoryObject.java
+++ src/logic/InventoryObject.java
@@ -1,8 +1,23 @@
 package logic;
 
-public class InventoryObject extends WorldObject{
+public class InventoryObject extends WorldObject {
+	
+	private int count;
 
 	public InventoryObject(String objectName) {
 		super(objectName);
+		count = 1;
+	}
+	
+	public void increment(){
+		count++;
+	}
+	
+	public void decrease(){
+		count--;
+	}
+
+	public int getCount() {
+		return count;
 	}
 }
